aff21f73e8aa1c4e7f5a0fe596ac02af821d2281
Gilvan Pedro <87739497+GilvanPedro@users.noreply.github.com>
2025-10-10 14:54:38 -0300

    Delete pdv-posto-de-combustivel directory

--- pdv-posto-de-combustivel/src/main/java/com/br/pdvpostocombustivel/domain/exception/PessoaException.java
+++
@@ -1,7 +0,0 @@
-package com.br.pdvpostocombustivel.domain.exception;
-
-public class PessoaException extends RuntimeException {
-    public PessoaException(String message) {
-        super(message);
-    }
-}
